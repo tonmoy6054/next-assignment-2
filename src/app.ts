6334f9b64b57f1310979af77a3efb285748c428b
Tonmoy Ghosh <tonmoy.mec@gmail.com>
2023-11-29 12:13:11 +0600

    done

--- src/app.ts
+++ src/app.ts
@@ -1,17 +1,11 @@
 import express, { Application, Request, Response } from 'express';
 import cors from 'cors';
 import { userRoutes } from './app/modules/users/user.route';
+
 const app: Application = express();
 
 app.use(express.json());
-// app.use(cors());
-const corsConfig = {
-  origin: '',
-  credentials: true,
-  methods: ['GET', 'POST', 'PUT', 'DELETE'],
-};
-app.use(cors(corsConfig));
-app.options('', cors(corsConfig));
+app.use(cors());
 
 app.use('/api/users', userRoutes);
 
